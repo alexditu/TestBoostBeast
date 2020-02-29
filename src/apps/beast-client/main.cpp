b9056212bcb8505aed5515d82a04bf9b8ae6cdfe
Alex Ditu <ditu.alexandru@gmail.com>
2020-02-29 12:27:31 +0200

    added boost dep and configured vs code to use cmake

--- src/apps/beast-client/main.cpp
+++ src/apps/beast-client/main.cpp
@@ -1,11 +1,15 @@
 
 #include <log.h>
 
+#include <boost/asio/io_context.hpp>
+
 int main(int argc, char** argv)
 {
 	initLogger();
 
 	logD("Hello world");
+
+	boost::asio::io_context ioContext;
 
 	return 0;
 }
